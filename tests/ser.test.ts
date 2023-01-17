4dd5fa7c2bc91c3b36f6b649199dcb6218dd0cf4
James <james@prestwi.ch>
2023-01-17 15:34:37 -0500

    test: minor ser test fix

--- tests/ser.test.ts
+++ tests/ser.test.ts
@@ -4,7 +4,7 @@ import * as ethers from 'ethers';
 import { MevTxBuilder } from '../src';
 
 const SERIALIZED =
-  '{"chainId":31337,"wallet":"0x0000000000000000000000000000000000000000","to":"0x0000000000000000000000000000000000000000","data":"0x1234abcd","value":"0x1f4","delegate":true,"tip":"-0x2328","maxBaseFee":"0x53","timing":"0x1e3c5","nonce":"0x10","r":"0x32c76b09","s":"0x136f75","v":18}';
+  '{"chainId":31337,"wallet":"0x0000000000300000000000000000000000000000","to":"0x0000000000000050000000000000000000000000","data":"0x1234abcd","value":"0x1f4","delegate":true,"tip":"-0x2328","maxBaseFee":"0x53","timing":"0x1e3c5","nonce":"0x10","r":"0x32c76b09","s":"0x136f75","v":18}';
 
 // describe('toJson', () => {});
 
@@ -15,7 +15,7 @@ describe('fromJson', () => {
       ethers.providers.getDefaultProvider(),
       SERIALIZED,
     );
-    console.log(b);
     expect(b).toBeDefined();
+    expect(b.nonce?.eq(16)).toBe(true);
   });
 });
